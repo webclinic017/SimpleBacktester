f7e0c79a1dd78c907d5304a023d4f9ef4f5c6339
gio <[omitted]>
2020-12-28 11:54:39 +0100

    Fixes scripts imports

--- scripts/download_historical_ticks.py
+++ scripts/download_historical_ticks.py
@@ -4,8 +4,8 @@ CLI script to download historical ticks. Works for both active and expired futur
 
 if __name__ == "__main__":
     
-    from gulo.historical_data.utils.ticks import download_and_store_hist_ticks
-    from gulo.utils.utils import start_ib
+    from src.historical_data.utils.ticks import download_and_store_hist_ticks
+    from src.utils.ib import start_ib
     import datetime
     from ib_insync import Contract, ContractDetails, Future
     from typing import List, Optional
