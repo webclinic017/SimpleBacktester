d959051d67c5cd811935fa5cea974d79144fe88a
gio <[omitted]>
2020-12-28 13:21:41 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ setup(
     ],
     package_dir={"": "src"},
     install_requires=[
-        "ib-insync",
-        "matplotlib",
-        "numpy",
-        "pandas",
-        "psycopg2"
+        "ib-insync==0.9.64",
+        "matplotlib==3.2.1",
+        "numpy==1.19.4",
+        "pandas==1.0.4",
+        "psycopg2-binary==2.8.5"
     ],
 )
