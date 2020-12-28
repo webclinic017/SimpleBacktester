7c7e8b379a7d37daffbe7d56a277ccc3d528f1c6
gio <[omitted]>
2020-12-28 14:07:37 +0100

    package with pip

--- setup.py
+++ setup.py
@@ -12,6 +12,13 @@ setuptools.setup(
     long_description=long_description,
     url="https://github.com/gipaetusb/SimpleBacktester",
     packages=setuptools.find_packages(),
+    install_requires=[
+        "ib-insync==0.9.64",
+        "matplotlib==3.2.1",
+        "numpy==1.19.4",
+        "pandas==1.0.4",
+        "psycopg2-binary==2.8.5",
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
